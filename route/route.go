96b3ca17152338433378cf6a541548bd518fb122
Cyantosh0 <shresthasantosh799@gmail.com>
2021-07-30 10:09:55 +0545

    Add 'check' endpoint

--- route/route.go
+++ route/route.go
@@ -8,11 +8,13 @@ import (
 
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
+
+	r.GET("/check", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Up and running"})
+	})
+
 	user := r.Group("/user")
 	{
-		user.GET("/apple/b", func(c *gin.Context) {
-			c.JSON(200, gin.H{"message": "Up and running"})
-		})
 		user.GET("/", controller.GetUsers)
 		user.POST("/", controller.CreateUser)
 		user.GET("/:id", controller.GetUserByID)
